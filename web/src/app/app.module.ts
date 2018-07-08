3b2c4d55384e791d6ce3df8c88fc0ee2dec8521b
klimentru1986 <kliment1986@inbox.ru>
2018-07-08 18:03:21 +0300

    main-page template

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule],
+  imports: [BrowserModule, BrowserAnimationsModule, SharedModule],
   providers: [],
   bootstrap: [AppComponent]
 })
