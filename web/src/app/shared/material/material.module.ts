a5e1f8e69bc0dd6ed9db381b09c169e1036ea5fb
klimentru1986 <kliment1986@inbox.ru>
2018-07-08 18:08:09 +0300

    toolbar

--- web/src/app/shared/material/material.module.ts
+++ web/src/app/shared/material/material.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+  imports: [CommonModule, MatToolbarModule],
+  declarations: [],
+  exports: [MatToolbarModule]
 })
-export class MaterialModule { }
+export class MaterialModule {}
