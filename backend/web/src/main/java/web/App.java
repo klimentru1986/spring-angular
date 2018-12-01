af0b1862bff6f3a029613b87a3c2b6df58cb0412
kliment <kliment@MacBook-Pro.local>
2018-12-01 23:37:06 +0300

    spring security basic

--- backend/web/src/main/java/web/App.java
+++ backend/web/src/main/java/web/App.java
@@ -3,9 +3,10 @@ package web;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class App {
 
     public static void main(String[] args) {
